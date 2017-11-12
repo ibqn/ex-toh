b0d474d32a9068adf10aabc2945c05183c19e029
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-11-12 09:49:39 +0100

    add default not found page with response as json

--- src/index.js
+++ src/index.js
@@ -13,13 +13,14 @@ app.server = http.createServer(app)
 
 app.use(morgan('dev'))
 
-mongoose.connect('mongodb://localhost/heroes', { useMongoClient: true })
+// mongoose.connect('mongodb://localhost/heroes', { useMongoClient: true })
+mongoose.connect(config.dbConnection, { useMongoClient: true })
 mongoose.Promise = global.Promise
 // mongoose.Promise = bluebird
 
 const db = mongoose.connection
 db.on('error', console.log.bind(console, 'connection error:'))
-db.once('open', () => {
+db.once('open', async () => {
   console.log('successfully connected to the mondodb server')
 })
 
@@ -27,7 +28,7 @@ app.use(bodyParser.json({
 	limit: config.bodyLimit
 }))
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.json({
     status: 'ok',
     result: `Express js is running on port ${config.port}`
@@ -35,6 +36,13 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api', api)
+
+app.use(async (req, res, /*next*/) => {
+  res.status(404).format({
+    // json: () => res.json({ error: 'Not found' }),
+    default: () => res.json({ error: 'Not found' })
+  })
+})
 
 app.server.listen(process.env.PORT || config.port, () => {
   console.log(`Started on port ${app.server.address().port}`)
